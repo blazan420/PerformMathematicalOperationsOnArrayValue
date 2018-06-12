14fb96e195855709408bfe94aca1ffdf88e4da76
kevin <kevinjobrien420@gmail.com>
2018-06-11 21:35:55 -0500

    #178 PerformMathematicalOperationsOnArrayValue //Coding

--- app/src/main/java/com/example/kevin/performmathematicaloperationsonarrayvalue/MainActivity.java
+++ app/src/main/java/com/example/kevin/performmathematicaloperationsonarrayvalue/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.performmathematicaloperationsonarrayvalue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtTotal = (TextView)findViewById(R.id.txtTotal);
+        TextView txtCalculations = (TextView)findViewById(R.id.txtCalculations);
+
+        String oldText;
+
+        int[] integerArray = {2, 54, 34, 76, 23, 4 , 7, 86 ,4, 65};
+        int totolValue =0;
+
+        for (int index = 0; index < integerArray.length; index++){
+
+
+
+
+        }
+
+
+
     }
 }
